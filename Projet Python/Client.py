f3b73910e4ee72f7c691605f67e91cc73f26447f
@Samsam03 <samuel.castre82@gmail.com>
2021-10-26 17:15:04 +0200

    Les 2 machines peuvent mtn communiquer

--- Projet Python/Client.py
+++ Projet Python/Client.py
@@ -1,11 +1,27 @@
 import socket
+import threading
+import time
 
 host = "127.0.0.1"
 port = 65535
 
+class ThreadReception(threading.Thread):
+    def __init__(self, stop, server):
+        threading.Thread.__init__(self)
+        self.stop = stop
+        self.server = server
+
+    def run(self):
+        while(self.stop != b'fin'):
+            data = repr(s.recv(1024).decode(encoding='utf-8'))
+            time.sleep(0.08) ## Facilite la lecture d'affichage
+            print('\nRecu :', data)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
-    s.send(b'Hello World')
-    data = s.recv(1024)
+    r = ThreadReception("",s)
+    r.start()
 
-print('Recu :', repr(data))
+    while True:
+        msg = bytes(input("Envoyer un message : ").encode(encoding='utf-8'))
+        s.send(msg)
--- Projet Python/Server.py
+++ Projet Python/Server.py
@@ -1,17 +1,32 @@
 import socket
+import threading
+import time
+
 
 host = "127.0.0.1"
 port = 65535
+
+class ThreadReception(threading.Thread):
+    def __init__(self, stop, server):
+        threading.Thread.__init__(self)
+        self.stop = stop
+        self.server = server
+
+    def run(self):
+        while(self.stop != b'fin'):
+            time.sleep(0.08) ## Facilite la lecture d'affichage
+            data = self.server.recv(1024).decode('utf-8')
+            print('\nRecu :', data)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((host, port))
     s.listen()
     conn, addr = s.accept()
-
+    r = ThreadReception("",conn)
+    r.start()
     with conn:
         print("Connecté à", addr)
+
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.send(data)
+            msg = bytes(input("Envoyer un message : ").encode(encoding='utf-8'))
+            conn.send(msg)
