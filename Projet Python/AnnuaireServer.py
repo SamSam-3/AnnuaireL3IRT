59f2723949ac857efaf68ed1a2496c754d64cf09
Xayn <samuel.castre82@gmail.com>
2021-11-18 10:26:06 +0100

    Retry IP

--- Projet Python/AnnuaireServer.py
+++ Projet Python/AnnuaireServer.py
@@ -91,7 +91,7 @@ class ThreadConnexion(threading.Thread):
 
             self.server.close()
 
-server = Server(64030,"10.188.14.206")
+server = Server(64030,"46.193.6.210")
 
 def connServer(client,data):
     verif = 0
